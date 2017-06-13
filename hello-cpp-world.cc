a442937beb277c0e0384c2d941b8be13f9d7363b
San Jeong <21400684@handong.edu>
2017-06-13 11:37:45 +0000

    init

--- hello-cpp-world.cc
+++ hello-cpp-world.cc
@@ -1,5 +1,61 @@
-#include <iostream>
-
-int main() {
-    std::cout << "Hello World!" << std::endl;
+int main(int argc, char** argv) // 포인터로 vector 리스트에 집어넣어서 각자 다른 수행을 가능하게 한다
+{   
+    std::map<std::string, std::vector<Thing*>* > m_student_list;
+    
+    if(m_student_list.find("SIT22005") != m_student_list.end())
+    {
+        // do something
+        m_student_list["SIT22005"]->push_back(new Person("Hello1"));
+        m_student_list["SIT22005"]->push_back(new Person("Hello2"));
+        m_student_list["SIT22005"]->push_back(new Person("Hello3"));
+        m_student_list["SIT22005"]->push_back(new Person("Hello4"));
+    }
+    else
+    {
+        m_student_list.insert(std::pair<std::string, std::vector<Thing*>*>("SIT22005", new std::vector<Thing*>));
+    }
+    
+    
+    //Person* p_list;
+    std::vector<Thing*> vecList;
+    
+    //// Game World
+    vecList.push_back(new Person("Hello1"));
+    vecList.push_back(new Person("Hello2"));
+    vecList.push_back(new Animal("Hello3"));
+    vecList.push_back(new ICTer("Hello3"));
+    vecList.push_back(new Animal("Hello4"));
+    ////
+    
+    /****************************************/
+    for(int i = 0; i < vecList.size(); i++)
+    {
+     //   std::cout << vecList[i].get_name().c_str() << std::endl;
+     std::cout << vecList[i]->say_something().c_str() << std::endl;
+    }
+    
+    ///////////////////////////////////////////이럴떈 vector 보다 list가 빠르다 REALLOCATION 이 없기 때문에
+    std::list<Thing*> vecList;
+    
+    //// Game World
+    vecList.push_back(new Person("Hello1"));
+    vecList.push_back(new Person("Hello2"));
+    vecList.push_back(new Animal("Hello3"));
+    vecList.push_back(new ICTer("Hello3"));
+    vecList.push_back(new Animal("Hello4"));
+    ////
+    
+    /****************************************/
+    //for(int i = 0; i < vecList.size(); i++)
+    //{
+    // //   std::cout << vecList[i].get_name().c_str() << std::endl;
+    // std::cout << vecList[i]->say_something().c_str() << std::endl;
+    //}
+    
+    for(std::list<Thing*>::iterator iter = vecList.begin();
+        iter != vecList.end(); ++iter)
+    {
+        std::cout << (*iter)->say_something().c_str() << std::endl;
+    }
 }
+    /****************************************/
--- standard templete/vector.cpp
+++ standard templete/vector.cpp
@@ -1,22 +1,20 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <list>
+#include <map>
 
 class Thing
 {
 public:
     Thing(std::string name)
-    :m_name(name)
-    {
-        
-    }
-    
+    :m_name(name){ }
 public:
-    std::string& get_name()
-    {
-        return m_name;
-    }
-private:
+    std::string& get_name(){ return m_name; }
+    
+    virtual std::string say_something(){ return m_name; }
+    
+protected:
     std::string m_name;
 };
 
@@ -24,9 +22,13 @@ class Person: public Thing //inheritance
 {
 public:
     Person(std::string name)
-    :Thing(name)
-    {
-        
+    :Thing(name){}
+    
+    std::string say_something(){ 
+        std::string contents;
+        contents += "My name is ";
+        contents += m_name;
+        return contents; 
     }
 };
 
@@ -38,20 +40,76 @@ public:
     {
         
     }
+    
+    std::string say_something(){ 
+        std::string contents;
+        contents += "Wang Wang! ";
+        contents += m_name;
+        return contents; 
+    }
 };
+
+class ICTer: public Thing 
+{
+public:
+    ICTer(std::string name)
+    :Thing(name)
+    {
+        
+    }
+    
+    std::string say_something(){ 
+        std::string contents;
+        contents += "I love Programming 2 ";
+        contents += m_name;
+        return contents; 
+    }
+};
+
 
 int main(int argc, char** argv)
 {
+    std::map<std::string, std::vector<Thing*>* > m_student_list;
+    
+    if(m_student_list.find("SIT22005") != m_student_list.end())
+    {
+        // do something
+        m_student_list["SIT22005"]->push_back(new Person("Hello1"));
+        m_student_list["SIT22005"]->push_back(new Person("Hello2"));
+        m_student_list["SIT22005"]->push_back(new Person("Hello3"));
+        m_student_list["SIT22005"]->push_back(new Person("Hello4"));
+    }
+    else
+    {
+        m_student_list.insert(std::pair<std::string, std::vector<Thing*>*>("SIT22005", new std::vector<Thing*>));
+    }
+    
+    
+    
     //Person* p_list;
-    std::vector<Thing> vecList;
+    std::list<Thing*> vecList;
     
-    vecList.push_back(Person("Hello1"));
-    vecList.push_back(Person("Hello2"));
-    vecList.push_back(Animal("Hello3"));
-    vecList.push_back(Animal("Hello4"));
+    //// Game World
+    vecList.push_back(new Person("Hello1"));
+    vecList.push_back(new Person("Hello2"));
+    vecList.push_back(new Animal("Hello3"));
+    vecList.push_back(new ICTer("Hello3"));
+    vecList.push_back(new Animal("Hello4"));
+    ////
     
-    for(int i = 0; i < vecList.size(); i++)
-        std::cout << vecList[i].get_name().c_str() << std::endl;
+    /****************************************/
+    //for(int i = 0; i < vecList.size(); i++)
+    //{
+    // //   std::cout << vecList[i].get_name().c_str() << std::endl;
+    // std::cout << vecList[i]->say_something().c_str() << std::endl;
+    //}
+    
+    for(std::list<Thing*>::iterator iter = vecList.begin();
+        iter != vecList.end(); ++iter)
+    {
+        std::cout << (*iter)->say_something().c_str() << std::endl;
+    }
+    /****************************************/
     
     return 0;
 }
